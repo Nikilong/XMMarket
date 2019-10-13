ae99a474ea7ab96c1b25438c889cc7f333ed53c9
Niki <Niki@NikideMacBook.local>
2019-10-13 16:52:56 +0800

    增加热点新闻的接口

--- xmshopServer/viewModel/newsVM.js
+++ xmshopServer/viewModel/newsVM.js
@@ -38,11 +38,11 @@ let getNewsDetail = function(data) {
 let getNewsByChannel = function(data) {
     let params = data.PARAMS
     let resData = {}
-    let cityName= "%E5%B9%BF%E5%B7%9E"
+    let cityName= encodeURI("广州")
     if(params.cityName){
         cityName = params.cityName
     }
-    console.log("---city:",decodeURI(cityName),params.id)
+    console.log("---city:",cityName,params.id)
     return new Promise((resolve, reject) => {
         request(`http://iflow.uczzd.cn/iflow/api/v1/channel/${params.id}?app=uc-iflow&sp_gz=1&city_name=${cityName}&recoid=4780413596505591035&ftime=1488170672207&method=new&count=20&no_op=0&auto=0&content_ratio=0&_tm=1488170686131&sign=bTkwBf9iQB%2F%2B%2F%2BHW3ogpeRxXTASXxQkcussc021pvCbY8PhRcDeSzLbM&sc=&uc_param_str=dnnivebichfrmintcpgieiwidsudsvli&dn=20151536891-fdd83270&ni=bTkwBTRiltTM%2FB%2BoM9%2BSzKFTH%2Fw9my2pBssWYs9s4gujq%2F0%3D&ve=10.9.6.755&bi=997&fr=iphone&mi=iPhone8%2C1&nt=2&cp=isp%3A%E7%94%B5%E4%BF%A1%3Bprov%3A%E5%B9%BF%E4%B8%9C%3Bcity%3A%3Bna%3A%E4%B8%AD%E5%9B%BD%3Bcc%3ACN%3Bac%3A&ei=bTkwBcdiqTSojXfM8KyHGJdaY7Z1jg5t1b9yZ4hg%2B347oiyKCUxxdgEfSBZKXw%3D%3D&ds=bTkwBepirf3loQ6QrJ76djgUEWU26yiBFIsxt2oP%2FNcXoQ%3D%3D&ud=bTkwBQ1isdG7gH35p53uwnMKCofF7Sj45ZQil%2B8I6hf4tQ%3D%3D&sv=app&li=g9qKibW4p5yD14qJsbysy4%2Fa04ji5%2FOd7ui%2F`, function (error, response, body) {
             if (!error && response.statusCode == 200) {
@@ -82,7 +82,6 @@ let searchNews = function(data) {
         keyword = encodeURI(params.keyword)
     }
     let weburl =`http://mbsug.ssl.so.com/sug?channel=type_webpage&caller=strict&sensitive=strict&srcg=sina_shoulang_act&word=$${keyword}&callback=__jp4`;
-    console.log(weburl)
     return new Promise((resolve, reject) => {
         request(weburl, function (error, response, body) {
             if (!error && response.statusCode == 200) {
@@ -92,6 +91,41 @@ let searchNews = function(data) {
                 try{
                     let str = body.substring(body.indexOf("(") + 1,body.indexOf(")"))
                     dict = JSON.parse(str)
+                }catch(e){console.log(e)}
+                resData.status = 'success'
+                resData.RESULT = dict
+            }else{
+                resData.status = 'failed'
+                resData.RESULT = '无法获取数据'
+                
+            }
+            resolve(resData)
+        })
+    }).catch(e => {
+        console.log(e)
+        let resData = {}
+        resData.status = "failed"
+        resData.msg = e.message
+        return resData
+    })
+}
+
+
+// 查询热点新闻
+let getHotNews = function(data) {
+    console.log("------",data.SERVICE)
+    let resData = {}
+    let weburl = `https://so.sina.cn/channel/360/search`
+    return new Promise((resolve, reject) => {
+        request(weburl, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                // console.log(body) // Show the HTML for the baidu homepage.
+                // 将string类型的body数据转为JSON
+                let dict = ""
+                try{
+                    // 提取目标json
+                    let reg = /{"word_list":(.*),"focuspic_list"/
+                    dict = reg.exec(body)[1]
                 }catch(e){console.log(e)}
                 resData.status = 'success'
                 resData.RESULT = dict
